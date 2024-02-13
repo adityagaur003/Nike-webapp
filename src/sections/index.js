eedd4fcf6dbd981cb65b78c110833b6e45ea93c9
ADITYA GAUR <aditya@ADITYAs-MacBook-Air.local>
2024-02-13 18:33:40 +0530

    sections & logo

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import SpecialOffers from './SpecialOffers';
+import CustomerReviews from './CustomerReviews';
+import SuperQuality from './SuperQuality';
+
+export{
+    Hero,
+    PopularProducts,
+    Services,
+    Subscribe,
+    Footer,
+    SpecialOffers,
+    CustomerReviews,
+    SuperQuality
+}
